0ac24c9c3bc1872e21526f51dfc05eb0a8e886ef
Julien David <jucodavid@gmail.com>
2025-12-30 13:57:04 +0000

    task5 not finished

--- SiLagoNN/phy/scr/floorplan_constrained.tcl
+++ SiLagoNN/phy/scr/floorplan_constrained.tcl
@@ -1,12 +1,12 @@
 set margin 20
-set width 150
+set width  165
 set height 200
 
 create_floorplan -site SC8T_104CPP_CMOS22FDX -core_size [expr {8*$width}] [expr {2*$height}] $margin $margin $margin $margin -no_snap_to_grid
 
 for {set i 0} {$i < 8} {incr i} {
 #Top row
-    set x1 [expr {double($margin + $width * $i)}] 
+    set x1 [expr {double($margin + $width * $i)}]
     set y1 [expr {double($margin + $height    )}]
     set x2 [expr {double($x1 + $width )}]
     set y2 [expr {double($y1 + $height)}]
@@ -21,3 +21,14 @@ for {set i 0} {$i < 8} {incr i} {
     set cell [lindex ${all_partition_hinst_list} $j ]
     create_boundary_constraint -type fence -hinst $cell -rects [list [list $x1 $y1 $x2 $y2]]
 }
+
+update_floorplan_obj -obj Silago_top_inst_3_0 -rects {514.937 219.684 679.881 420.004}
+update_floorplan_obj -obj Silago_bot_inst_3_1 -rects {514.933 20.0 679.877 219.68}
+update_floorplan_obj -obj Silago_top_inst_4_0 -rects {679.879 219.675 844.823 419.995}
+update_floorplan_obj -obj Silago_bot_inst_4_1 -rects {679.88 20.009 844.824 219.689}
+update_floorplan_obj -obj Silago_top_inst_5_0 -rects {844.819 219.652 1009.763 419.972}
+update_floorplan_obj -obj Silago_bot_inst_5_1 -rects {844.833 20.0 1009.777 219.68}
+update_floorplan_obj -obj Silago_top_inst_6_0 -rects {1009.763 219.68 1174.707 420.0}
+update_floorplan_obj -obj Silago_bot_inst_6_1 -rects {1009.759 19.969 1174.703 219.649}
+update_floorplan_obj -obj Silago_top_r_corner_inst_7_0 -rects {1174.711 219.68 1339.655 420.0}
+update_floorplan_obj -obj Silago_bot_r_corner_inst_7_1 -rects {1174.696 20.019 1339.64 219.699}
--- SiLagoNN/phy/scr/partition.tcl
+++ SiLagoNN/phy/scr/partition.tcl
@@ -1,9 +1,17 @@
-foreach part_hinst ${all_partition_hinst_list} {
+foreach part_hinst ${master_partition_hinst_list} {
     create_partition -hinst ${part_hinst} -core_spacing 0.0 0.0 0.0 0.0 -rail_width 0.0 -min_pitch_left 2 \
                      -min_pitch_right 2 -min_pitch_top 2 -min_pitch_bottom 2 -reserved_layer {1 2 3 4 5 6 7 8 9} \
                      -pin_layer_top {3 5 7 9} -pin_layer_left {2 4 6 8} -pin_layer_bottom {3 5 7 9} \
                      -pin_layer_right {2 4 6 8} -place_halo 10.0 0.0 0.0 0.0 -route_halo 10.0 -route_halo_top_layer 9 \
                      -route_halo_bottom_layer 1
+}
+
+foreach part_hinst ${clone_partition_top_hinst_list} {
+    create_partition -hinst ${part_hinst} -copy_from Silago_top_0
+}
+
+foreach part_hinst ${clone_partition_bot_hinst_list} {
+    create_partition -hinst ${part_hinst} -copy_from Silago_bot_0
 }
 
 align_partition_clones -update_user_grid -pg_horizontal_grid -pg_vertical_grid
@@ -15,8 +23,8 @@ set_db budget_virtual_opt_engine none
 set_db budget_constant_model true
 set_db budget_include_latency true
 
-create_timing_budget -partitions ${partition_module_list}
+create_timing_budget -partitions ${master_partition_module_list}
 
 commit_partitions
 
-write_partitions -dir ${PART_DIR} -def
+write_partitions ${master_partition_module_list} -dir ${PART_DIR} -def
