b36e2fe869ac7741623bdc5bbe0b6d758979f656
Julien David <jucodavid@gmail.com>
2025-12-31 07:24:55 +0000

    modified script

--- SiLagoNN/phy/scr/partition.tcl
+++ SiLagoNN/phy/scr/partition.tcl
@@ -1,3 +1,4 @@
+if {0} {
 foreach part_hinst ${master_partition_hinst_list} {
     create_partition -hinst ${part_hinst} -core_spacing 0.0 0.0 0.0 0.0 -rail_width 0.0 -min_pitch_left 2 \
                      -min_pitch_right 2 -min_pitch_top 2 -min_pitch_bottom 2 -reserved_layer {1 2 3 4 5 6 7 8 9} \
@@ -28,3 +29,30 @@ create_timing_budget -partitions ${master_partition_module_list}
 commit_partitions
 
 write_partitions ${master_partition_module_list} -dir ${PART_DIR} -def
+}
+
+
+
+
+foreach part_hinst ${all_partition_hinst_list} {
+    create_partition -hinst ${part_hinst} -core_spacing 0.0 0.0 0.0 0.0 -rail_width 0.0 -min_pitch_left 2 \
+                     -min_pitch_right 2 -min_pitch_top 2 -min_pitch_bottom 2 -reserved_layer {1 2 3 4 5 6 7 8 9} \
+                     -pin_layer_top {3 5 7 9} -pin_layer_left {2 4 6 8} -pin_layer_bottom {3 5 7 9} \
+                     -pin_layer_right {2 4 6 8} -place_halo 10.0 0.0 0.0 0.0 -route_halo 10.0 -route_halo_top_layer 9 \
+                     -route_halo_bottom_layer 1
+}
+
+align_partition_clones -update_user_grid -pg_horizontal_grid -pg_vertical_grid
+
+assign_partition_pins
+assign_io_pins
+
+set_db budget_virtual_opt_engine none
+set_db budget_constant_model true
+set_db budget_include_latency true
+
+create_timing_budget -partitions ${all_partition_module_list}
+
+commit_partitions
+
+write_partitions -dir ${PART_DIR} -def
