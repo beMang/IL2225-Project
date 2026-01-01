51e52b74fcb17d7476fce8a102dbbb505bb853ff
Ikhtiar Hossain <120584860+ikhtiarhossain@users.noreply.github.com>
2026-01-01 14:05:50 +0000

    script task 2 dc_flat.tcl

--- SiLagoNN/syn/scr/dc_flat.tcl
+++ SiLagoNN/syn/scr/dc_flat.tcl
@@ -1,30 +1,41 @@
-################################################################################
-# Flat logic synthesis script
-################################################################################
-#
-# This script is meant to be executed with the following directory structure
-#
-# project_top_folder
-# |
-# |- db: store output data like mapped designs or physical files like GDSII
-# |
-# |- phy: physical synthesis material (scripts, pins, etc)
-# |
-# |- rtl: contains rtl code for the design, it should also contain a
-# |       hierarchy.txt file with the all the files that compose the design
-# |
-# |- syn: logic synthesis material (this script, SDC constraints, etc)
-# |
-# |- sim: simulation stuff like waveforms, reports, coverage etc.
-# |
-# |- tb: testbenches for the rtl code
-# |
-# |- exe: the directory where it should be executed. This keeps all the temp files
-#         created by DC in that directory
-#
-#
-# The standard way of executing the is from the project_top_folder
-# with the following command
-#
-# $ dc_shell -f ../syn/dc_flat.tcl
-################################################################################
+# Task 2: Flat synthesis for multiple clock periods
+# Run in exe/task_2/ dc_shell:
+# source ../../syn/scr/dc_flat.tcl
+
+set SOURCE_DIR	"../../rtl"
+set SYN_DIR	"../../syn"
+set OUT_DIR	"../../syn/db/task_2"
+set REPORT_DIR	"../../syn/rpt/task_2"
+set TOP_NAME drra_wrapper
+
+set periods {20000 15000 8000 4000 2000}
+foreach T $periods {
+	remove_design -all
+	source ../../syn/synopsys_dc.setup
+
+	set hierarchy_files [split [read [open ${SOURCE_DIR}/${TOP_NAME}_hierarchy.txt r]] "\n"]
+	foreach filename [lrange ${hierarchy_files} 0 end-1] {
+		analyze -format VHDL -lib WORK "${SOURCE_DIR}/${filename}"
+	}
+	elaborate ${TOP_NAME}
+	link
+	
+	# 50% duty cycle with 1% jitter/skew excluding reset signal
+	set half_T [expr $T / 2]
+	set unc [expr $T / 100]
+	create_clock -name "clk" -period ${T} -waveform "0 ${half_T}" [get_ports clk]
+	set_clock_uncertainty ${unc} [get_clock clk]
+	set_false_path -from [get_port rst_n]
+
+	compile -map_effort medium
+
+	report_area > ${REPORT_DIR}/${TOP_NAME}_area_${T}ps.txt
+	report_cell > ${REPORT_DIR}/${TOP_NAME}_cells_${T}ps.txt
+	report_timing > ${REPORT_DIR}/${TOP_NAME}_timing_${T}ps.txt
+	report_power > ${REPORT_DIR}/${TOP_NAME}_power_${T}ps.txt
+	report_constraints > ${REPORT_DIR}/${TOP_NAME}_con_${T}ps.txt
+
+	# database and netlist for physical synthesis    
+	write -hierarchy -format ddc -output ${OUT_DIR}/${TOP_NAME}_${T}ps.ddc
+	write -hierarchy -format verilog -output ${OUT_DIR}/${TOP_NAME}_${T}ps.v
+}
