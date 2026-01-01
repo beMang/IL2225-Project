b9a38a8d7f2c9e2379e478e085807e81492e6c63
Ikhtiar Hossain <120584860+ikhtiarhossain@users.noreply.github.com>
2026-01-01 13:58:08 +0000

    script and run task 1 simulation.tcl

--- SiLagoNN/tb/vec_add/simulation.tcl
+++ SiLagoNN/tb/vec_add/simulation.tcl
@@ -1,8 +1,32 @@
+#	From /SiLagoNN/tb/vec_add run: 
+#	vsim -do simulation.tcl
+
 vlib work
 vlib dware
 
-#1. Compile dware libraries into "dware" and the design into "work"
+set TOP_NAME silagonn
+set RUN_TIME "1000ns"
+  
+set SOURCE_DIR "../../rtl"
+set TB_DIR "."
 
-#2. Compile testbench. 
+set dware_hierarchy_files [split [read [open ${SOURCE_DIR}/dware_hierarchy.txt r]] "\n"]
+set hierarchy_files [split [read [open ${SOURCE_DIR}/${TOP_NAME}_hierarchy.txt r]] "\n"]
 
-#3. Run simulation. 
+#	Compile
+foreach filename [lrange ${dware_hierarchy_files} 0 end-1] {
+	vcom -2008 -work dware ${SOURCE_DIR}/${filename}
+}
+
+foreach filename [lrange ${hierarchy_files} 0 end-1] {
+	vcom -2008 -work work ${SOURCE_DIR}/${filename}
+}
+
+vcom -2008 -work work ${TB_DIR}/const_package.vhd
+vcom -2008 -work work ${TB_DIR}/testbench.vhd
+
+#	Run sim
+vsim -voptargs=+acc work.testbench
+do wave.do
+
+run ${RUN_TIME}
